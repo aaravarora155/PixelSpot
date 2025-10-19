ce18c5c04a9d0b0461d1f615651fb83ecb73ec99
aaravarora155 <aarav2607@gmail.com>
2025-10-19 15:38:20 +0100

    Update contactFrontend.js

--- contactFrontend.js
+++ contactFrontend.js
@@ -1,24 +1,24 @@
 function valid(email){
-    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return pattern.test(email);
+const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+return pattern.test(email);
 }
 
 
 $("button").click(async function() {
-    const name = $(".user").val();
-    const email = $(".emailEntered").val();
-    const issue = $(".issueText").val();
-    if (valid(email)){
-        alert("Request Sent, Copy has been sent    to the email you entered!");
-        const response = await     fetch("https://trotty-inexpressively-rosette  .ngrok-free.dev/send-email", {
-            method: "POST",
-            headers: { "Content-Type":  "application/json" }, 
-            body: JSON.stringify({ name, email,  issue })
-        });
+const name = $(".user").val();
+const email = $(".emailEntered").val();
+const issue = $(".issueText").val();
 
-        const data = await response.json();
-    }
-    else{
-        alert("Email is not in an approved format");
-    }
+if (valid(email)){
+alert("Request Sent, Copy has been sent    to the email you entered!");
+   
+const response = await     fetch("https://trotty-inexpressively-rosette .ngrok-free.dev/send-email", {
+method: "POST",
+headers: { "Content-Type":  "application/json" }, body: JSON.stringify({ name, email,  issue })
+});
+const data = await response.json();
+}
+else{
+alert("Email is not in an approved format");
+}
 });
