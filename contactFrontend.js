228259cb0128fbf2d6428992f64ee06e82bdba1c
aaravarora155 <aarav2607@gmail.com>
2025-10-19 18:19:21 +0100

    Refactor email validation and add length checks

--- contactFrontend.js
+++ contactFrontend.js
@@ -1,24 +1,34 @@
-function valid(email){
-const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-return pattern.test(email);
+function valid(email) {
+    const pattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return pattern.test(email);
 }
-
-
+function checkLength(name){
+    if (name.length===0){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
 $("button").click(async function() {
-const name = $(".user").val();
-const email = $(".emailEntered").val();
-const issue = $(".issueText").val();
+    const name = $(".user").val();
+    const email = $(".emailEntered").val();
+    const issue = $(".issueText").val();
 
-if (valid(email)){
-alert("Request Sent, Copy has been sent to the email you entered!");
-   
-const response = await fetch("https://trotty-inexpressively-rosette .ngrok-free.dev/send-email", {
-method: "POST",
-headers: { "Content-Type":  "application/json" }, body: JSON.stringify({ name, email,  issue })
-});
-const data = await response.json();
-}
-else{
-alert("Email is not in an approved format");
-}
+    if (valid(email)===true && checkLength(name)===true && checkLength(issue)===true){
+        
+        alert("Request Sent, Copy has been sent to the email you entered!");
+        const response = await fetch("https://trotty-inexpressively-rosette.ngrok-free.dev/send-email", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ name, email, issue })
+        });
+
+        const data = await response.json();
+    }
+    else{
+        alert("Please enter a valid email address and ensure all fields are filled.");
+    }
+    
+    
 });
