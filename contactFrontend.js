f0d096b9e60ae1e3d2549da6e7565c61a221410f
aaravarora155 <aarav2607@gmail.com>
2025-10-19 15:41:53 +0100

    Update contactFrontend.js

--- contactFrontend.js
+++ contactFrontend.js
@@ -10,9 +10,9 @@ const email = $(".emailEntered").val();
 const issue = $(".issueText").val();
 
 if (valid(email)){
-alert("Request Sent, Copy has been sent    to the email you entered!");
+alert("Request Sent, Copy has been sent to the email you entered!");
    
-const response = await     fetch("https://trotty-inexpressively-rosette .ngrok-free.dev/send-email", {
+const response = await fetch("https://trotty-inexpressively-rosette .ngrok-free.dev/send-email", {
 method: "POST",
 headers: { "Content-Type":  "application/json" }, body: JSON.stringify({ name, email,  issue })
 });
