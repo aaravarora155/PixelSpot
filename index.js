2fb3ba689ee5c9e8d0ae382bba11c44906b1d9dd
aaravarora155 <aarav2607@gmail.com>
2025-12-18 18:39:29 +0000

    Changed Menu

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 $("#top").css("border-radius","20px 20px 0px 0px");
 $("#bottom").css("border-radius","0px 0px 20px 20px");
+$("details").on("mouseover", function(){
+    $("details").attr("open",true);
+})
+$(".close").on("mouseout", function(){
+    $("details").attr("open",false);
+})
