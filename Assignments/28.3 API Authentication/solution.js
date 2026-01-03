7015680a96b8d9d22aa67e10b75fceaac370ceda
aaravarora155 <aarav2607@gmail.com>
2026-01-03 23:15:15 +0000

    Updated packages, dependencies, and assignments.

---
+++ Assignments/28.3 API Authentication/solution.js
@@ -0,0 +1,70 @@
+import express from "express";
+import axios from "axios";
+
+const app = express();
+const port = 3000;
+const API_URL = "https://secrets-api.appbrewery.com";
+
+// TODO: Replace the values below with your own before running this file.
+const yourUsername = "testUser";
+const yourPassword = "testPass";
+const yourAPIKey = "2cc417f9-2f2b-41a7-a323-7a92236abd71";
+const yourBearerToken = "115e2a63-7d6f-46e5-a77b-df94da607436";
+
+app.get("/", (req, res) => {
+  res.render("index.ejs", { content: "API Response." });
+});
+
+app.get("/noAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/random");
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/basicAuth", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/all?page=2", {
+      auth: {
+        username: yourUsername,
+        password: yourPassword,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.get("/apiKey", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/filter", {
+      params: {
+        score: 5,
+        apiKey: yourAPIKey,
+      },
+    });
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+const config = {
+  headers: { Authorization: `Bearer ${yourBearerToken}` },
+};
+
+app.get("/bearerToken", async (req, res) => {
+  try {
+    const result = await axios.get(API_URL + "/secrets/2", config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- Projects/Blog Website/index.js
+++ Projects/Blog Website/index.js
@@ -9,13 +9,11 @@ const port = 3000;
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let counter=0;
 let authors = [];
 let titles = [];
 
 function storeFile(name,title,body) {
-    let postPath = 'posts/' + name + '.txt';
-    counter += 1;
+    let postPath = 'posts/' + title + '.txt';
     const message = "Author: " + name + "\n\n" + "Title: " + title + "\n\n" + "Body: " + body;
     fs.writeFile(postPath, message, (err) => {
         if (err) throw err;
@@ -43,9 +41,13 @@ app.post("/edit", (req, res) => {
 
 })
 app.post("/delete", (req, res) => {
-
+    console.log(authors+"-"+titles);
+    authors=authors.splice(authors.indexOf(req.body["author"]),1);
+    titles=titles.splice(titles.indexOf(req.body["title"]),1);
+    console.log(authors+"-"+titles);
+    res.render("index.ejs",{"authors":authors,"titles":titles});
 })
-app.post("/view", (req, res) => {
+app.get("/view", (req, res) => {
 
 })
 app.listen(port, () => {
