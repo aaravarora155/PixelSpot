b1945a81e639c604f571f09683921194f964bfab
aaravarora155 <aarav2607@gmail.com>
2026-04-11 21:08:11 +0100

    SQL Files

---
+++ Assignments/33 PostgreSQL/world.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS capitals;
+
+CREATE TABLE IF NOT EXISTS capitals(
+    id SERIAL PRIMARY KEY,
+    country VARCHAR(100),
+    capital VARCHAR(100)
+);
+
+--Edit Path to your CSV file
+COPY capitals FROM 'C:\Web Applications\Hosting\Assignments\33 PostgreSQL\capitals.csv' DELIMITER ',' CSV HEADER;
