e6cd8c53bb0793dd3e79b89bd2379a5753296d5c
aaravarora155 <aarav2607@gmail.com>
2026-04-15 19:04:18 +0100

    fixes

--- Projects/Secrets Project/index.js
+++ Projects/Secrets Project/index.js
@@ -11,16 +11,16 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index");
+    res.sendFile(__dirname + "/public/index.html");
 })
 
 app.post("/check", (req, res) => {
     const password = req.body["password"];
     if (password === "ILoveProgramming") {
-        res.sendFile(__dirname + "/public/secret");
+        res.sendFile(__dirname + "/public/secret.html");
     }
     else {
-        res.sendFile(__dirname + "/public/index");
+        res.sendFile(__dirname + "/public/index.html");
     }
 })
 export default app;
