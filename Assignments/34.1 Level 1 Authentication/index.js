fe0816affcf0dc126ec477e3825dfe67de0f28fb
aaravarora155 <aarav2607@gmail.com>
2026-04-16 21:03:51 +0100

    Major Changes

---
+++ Assignments/34.1 Level 1 Authentication/index.js
@@ -0,0 +1,72 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+const db = new pg.Client({
+  connectionString:"postgresql://main:ZJPFb7FKnVL5JsK13DuavZc54VBeoyF1@dpg-d7fv57reo5us73b9hdo0-a.oregon-postgres.render.com/webdev_vsyb",
+  ssl:{
+    rejectUnauthorized: false
+  }
+});
+
+db.connect();
+
+async function dbInit() {
+  await db.query("CREATE TABLE IF NOT EXISTS userDetails (id SERIAL PRIMARY KEY, username VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL)");
+}
+
+await dbInit();
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  try{
+    const result = await db.query("SELECT * FROM userDetails WHERE username = $1", [username]);
+    if(result.rows.length > 0){
+      res.send("User already exists");
+    }else{
+      await db.query("INSERT INTO userDetails(username, password) VALUES ($1, $2)",[username, password]);
+    res.render("secrets.ejs");
+  }     
+  }catch(err){
+    console.log(err);
+  }
+
+});
+
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  try{
+    const result = await db.query("SELECT * FROM userDetails WHERE username = $1 AND password = $2", [username, password]);
+    if(result.rows.length > 0 && result != undefined){
+      res.render("secrets.ejs");
+    }else{
+      res.send("Invalid credentials");
+  }
+  }catch(err){
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
