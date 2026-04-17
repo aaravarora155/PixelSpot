8cdcf3a5e93163f42a2aaae541bff8eebcff2622
aaravarora155 <aarav2607@gmail.com>
2026-04-17 19:03:25 +0100

    Here We go Agian

--- Projects/Permalist Project/index.js
+++ Projects/Permalist Project/index.js
@@ -22,18 +22,20 @@ db.connect();
 let items = [];
 
 async function dbInit() {
-  await db.query("CREATE TABLE IF NOT EXISTS items(id SERIAL PRIMARY KEY, title VARCHAR(100))");
+  await db.query("CREATE TABLE IF NOT EXISTS items(id SERIAL PRIMARY KEY, title VARCHAR(100), email VARCHAR(255))");
 }
 
 await dbInit();
 
-async function getItems() {
-  const result = await db.query("SELECT * FROM items");
+async function getItems(email) {
+  const result = await db.query("SELECT * FROM items WHERE email = $1", [email]);
   items = result.rows;
+  return items;
 }
 
 app.get("/", async (req, res) => {
-  await getItems();
+  const email = req.user ? req.user.username : null;
+  await getItems(email);
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
@@ -41,22 +43,23 @@ app.get("/", async (req, res) => {
 });
 
 app.post("/add", async (req, res) => {
+  const email = req.user ? req.user.username : null;
   const item = req.body.newItem;
-  const id = await db.query("INSERT INTO items (title) VALUES ($1) RETURNING id", [item]);
-  items.push({ id: id.rows[0].id, title: item });
+  const id = await db.query("INSERT INTO items (title, email) VALUES ($1, $2) RETURNING id", [item, email]);
   res.redirect("./");
 });
 
 app.post("/edit", async (req, res) => {
+  const email = req.user ? req.user.username : null;
   const id = req.body.updatedItemId;
-  await db.query("UPDATE items SET title = $1 WHERE id = $2", [req.body.updatedItemTitle, id]);
-  await getItems();
+  await db.query("UPDATE items SET title = $1 WHERE id = $2 AND email = $3", [req.body.updatedItemTitle, id, email]);
   res.redirect("./");
 });
 
 app.post("/delete", async (req, res) => {
+  const email = req.user ? req.user.username : null;
   const id = req.body.deleteItemId;
-  await db.query("DELETE FROM items WHERE id = $1", [id]);
+  await db.query("DELETE FROM items WHERE id = $1 AND email = $2", [id, email]);
   res.redirect("./");
 });
 
