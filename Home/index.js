8d20548c50fb934bb6c6f0ee041ac3c10aaa3bbc
aaravarora155 <aarav2607@gmail.com>
2026-04-17 18:55:48 +0100

    Fixed some issues

--- Home/index.js
+++ Home/index.js
@@ -1,5 +1,5 @@
-$("#top").css("border-radius", "20px 20px 0px 0px");
-$("#bottom").css("border-radius", "0px 0px 20px 20px");
+$("#top").css("border-radius", "15px 15px 0px 0px");
+$("#bottom").css("border-radius", "0px 0px 15px 15px");
 
 // Removed mouseover logic as it interferes with mobile tap behavior. 
 // Standard <details> click behavior is more reliable for responsive design.
